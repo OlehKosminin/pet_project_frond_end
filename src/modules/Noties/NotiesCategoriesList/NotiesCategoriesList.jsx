7219b0883900ccb8145e18ddadc2e76e2e02d492
Vadim-27 <vadim.bulanyi@gmail.com>
2023-05-11 15:55:10 +0200

    markupNoties

--- src/modules/Noties/NotiesCategoriesList/NotiesCategoriesList.jsx
+++ src/modules/Noties/NotiesCategoriesList/NotiesCategoriesList.jsx
@@ -1,5 +1,15 @@
+import {css} from "./notiesCategoriesList.module.scss"
+import items from "./pets"; 
+console.log(items)
 const NotiesCategoriesList = () => {
-  return <div> NotiesCategoriesList</div>;
+  const pets = items.map(({ id, animal, text }) => (
+    <li key={id}>
+      <div>{animal}</div>
+      {text}
+      <button>Learn more</button>
+    </li>
+  ));
+  return <div><ul>{ pets}</ul></div>;
 };
 
 export default NotiesCategoriesList;
---
+++ src/modules/Noties/NotiesCategoriesList/pets.js
@@ -0,0 +1,76 @@
+const items = [
+  {
+    "id": "u9kgwNWGi3uUUwh0b8V49",
+    "animal": "Dog",
+    "text": "Eloquent JavaScript, Third Edition",
+    
+  },
+  {
+    "id": "YxhM4QDxPeA3SmPHcEZPJ",
+    "animal": "chinchilla",
+    "text": "Practical Modern JavaScript",
+    
+  },
+  {
+    "id": "zCd_RioNMOBaQwAXnc8Px",
+    "animal": "guinea pig",
+    "text": "Understanding ECMAScript 6",
+   
+  },
+  {
+    "id": "ck89qe3HriUDHe09TBoJ8",
+    "animal": "Hamster",
+    "text": "Speaking JavaScript",
+    
+  },
+  {
+    "id": "e1Tpn_I3wBkLREY6wG0lb",
+    "animal": "Rabbit",
+    "text": "Learning JavaScript Design Patterns",
+   
+  },
+  {
+    "id": "Op8ApLTY0-Vn2cR0vDIwG",
+    "animal": "Cat",
+    "text": "You Don't Know JS Yet",
+   
+  },
+  {
+    "id": "dkJSszfrRVtLVR_MfRqcu",
+    "animal": "chinchilla",
+    "text": "Pro Git",
+    
+  },
+  {
+    "id": "CTHE0f1kkWwqS5sL2tI8_",
+    "animal": "guinea pig",
+    "text": "Rethinking Productivity in Software Engineering",
+    
+  },
+  {
+    "id": "CTHE0f1kkWwqS5sL2tIcu",
+    "animal": "Hamster",
+    "text": "Rethinking Productivity in Software Engineering",
+   
+  },
+  {
+    "id": "CTHE0f1kkWwqS5sL2tI10",
+    "animal": "Rabbit",
+    "text": "Rethinking Productivity in Software Engineering",
+    
+  },
+  {
+    "id": "CTHE0f1kkWwqS5sL2tI11",
+    "animal": "Cat",
+    "text": "Rethinking Productivity in Software Engineering",
+    
+  },
+  {
+    "id": "CTHE0f1kkWwqS5sL2tI12",
+    "animal": "Dog",
+    "text": "Rethinking Productivity in Software Engineering",
+   
+  },
+]
+
+export default items;
