971c84504ce37229fd31be6fbac6d7abd569623f
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2023-05-16 19:54:15 +0300

    Update NotiesPage.jsx

--- src/pages/NotiesPage/NotiesPage.jsx
+++ src/pages/NotiesPage/NotiesPage.jsx
@@ -1,86 +1,12 @@
 import { Container } from "@mui/material";
-// import Loader from "../UserPage/loader";
 import NoticesCategoriesList from "../../modules/Noties/NotiesCategoriesList/NotiesCategoriesList";
 import NoticesCategoriesNav from "../../modules/Noties/NotiesCategoryNav/NotiesCategoryNav";
-// import { NoticesPagination } from "components/Notices/NoticesPagination/NoticesPagination";
 import NoticesSearch from "../../modules/Noties/NotiesSeach/NotiesSeach";
 import React, { useState } from "react";
 import css from "./notiesPage.module.scss";
-
-// import { useDispatch, useSelector } from "react-redux";
-// import { useParams } from "react-router-dom";
-// import {
-//   getNoticeByCategory,
-//   getUserNotices,
-// } from "../../redux/noties/noties-operations";
-// import {
-//   getNoteceIsLoadig,
-//   getNotices,
-//   getOwnNotices,
-// } from "../../redux/noties/noties-selector";
-// import { clearNotices } from "../../redux/noties/noties-slice";
-// import { current } from "../../redux/auth/auth-operations";
-// import { isUserLogin } from "../../redux/auth/auth-selector";
-// import Typography from "@mui/material/Typography";
+ography from "@mui/material/Typography";
 
 function NoticesPage() {
-  // const { categoryName } = useParams();
-  // const notices = useSelector(getNotices);
-  // const isLoading = useSelector(getNoteceIsLoadig);
-  // const favoriteNotices = useSelector(isUserLogin);
-  // const favoriteAds = favoriteNotices?.user?.favorite || [];
-  // const ownNotices = useSelector(getOwnNotices);
-  // const dispatch = useDispatch();
-
-// function NoticesPage() {
-// const { categoryName } = useParams();
-// const notices = useSelector(getNotices);
-// const isLoading = useSelector(getNoteceIsLoadig);
-// const favoriteNotices = useSelector(isUserLogin);
-// const favoriteAds = favoriteNotices?.user?.favorite || [];
-// const ownNotices = useSelector(getOwnNotices);
-// const dispatch = useDispatch();
-
-// const [query, setQuery] = useState("");
-
-// useEffect(() => {
-//   if (categoryName === "favorite") {
-//     dispatch(current());
-//   } else if (categoryName === "owner") {
-//     dispatch(getUserNotices());
-//   } else {
-//     dispatch(getNoticeByCategory({ category: categoryName }));
-//   }
-//   return () => dispatch(clearNotices([]));
-// }, [dispatch, categoryName]);
-
-// const dataToRender =
-//   categoryName === "favorite"
-//     ? favoriteAds
-//     : categoryName === "owner"
-//     ? ownNotices
-//     : notices.notices;
-
-// const handleSearch = (newQuery) => {
-//   setQuery(newQuery);
-// };
-  // useEffect(() => {
-  //   if (categoryName === "favorite") {
-  //     dispatch(current());
-  //   } else if (categoryName === "owner") {
-  //     dispatch(getUserNotices());
-  //   } else {
-  //     dispatch(getNoticeByCategory({ category: categoryName }));
-  //   }
-  //   return () => dispatch(clearNotices([]));
-  // }, [dispatch, categoryName]);
-
-  // const dataToRender =
-  //   categoryName === "favorite"
-  //     ? favoriteAds
-  //     : categoryName === "owner"
-  //     ? ownNotices
-  //     : notices.notices;
 
   const handleSearch = (newQuery) => {
     setQuery(newQuery);
@@ -103,17 +29,3 @@ function NoticesPage() {
       <NoticesCategoriesList />;
     </Container>
   );
-
-  // {isLoading ? (
-  //       <Loader />
-  //     ) : (
-  //       <NoticesCategoriesList
-  //         categoryName={categoryName}
-  //         data={dataToRender}
-  //       />
-  //     )}
-  //   </Container>
-  //   {/* <NoticesPagination /> */}
-  // </>
-
-// export default NoticesPage;
