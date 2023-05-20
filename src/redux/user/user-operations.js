7f17a9b65c2f4c448fc10267847b6948759664ef
AnBoyvan <andrew.boyvan1988@gmail.com>
2023-05-20 19:23:45 +0300

    upd

--- src/redux/user/user-operations.js
+++ src/redux/user/user-operations.js
@@ -1,27 +1,28 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import * as api from "../../shared/services/auth";
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://yourpet-backend.onrender.com/api';
+axios.defaults.baseURL = "https://yourpet-backend.onrender.com/api";
 
 export const accessToken = {
   set(accessToken) {
     axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
   },
   unset() {
-    axios.defaults.headers.common.Authorization = '';
+    axios.defaults.headers.common.Authorization = "";
   },
 };
 
 export const getCurrentUser = createAsyncThunk(
-  'user/current',
+  "user/current",
   async (_, { rejectWithValue, getState }) => {
     try {
       const value = getState().auth.accessToken;
       if (value === null) {
-        return rejectWithValue('Unable to fetch user');
+        return rejectWithValue("Unable to fetch user");
       }
       accessToken.set(value);
-      const { data } = await axios.get('user/current');
+      const { data } = await axios.get("user/current");
 
       return data;
     } catch (e) {
@@ -32,12 +33,12 @@ export const getCurrentUser = createAsyncThunk(
 );
 
 export const addToFavorites = createAsyncThunk(
-  'user/addFavorite',
+  "user/addFavorite",
   async (id, { rejectWithValue, getState }) => {
     try {
       const value = getState().auth.accessToken;
       if (value === null) {
-        return rejectWithValue('Unable to fetch user');
+        return rejectWithValue("Unable to fetch user");
       }
       accessToken.set(value);
       const { data } = await axios.post(`/user/favorite/${id}`);
@@ -49,15 +50,15 @@ export const addToFavorites = createAsyncThunk(
 );
 
 export const getFavorite = createAsyncThunk(
-  'user/getFavorite',
+  "user/getFavorite",
   async (_, { rejectWithValue, getState }) => {
     try {
       const value = getState().auth.accessToken;
       if (value === null) {
-        return rejectWithValue('Unable to fetch user');
+        return rejectWithValue("Unable to fetch user");
       }
       accessToken.set(value);
-      const { data } = await axios.get('/user/favorite');
+      const { data } = await axios.get("/user/favorite");
       return data.user.favorite;
     } catch (error) {
       return rejectWithValue(error);
@@ -66,7 +67,7 @@ export const getFavorite = createAsyncThunk(
 );
 
 export const deleteFromFavorite = createAsyncThunk(
-  'user/deleteFavorite',
+  "user/deleteFavorite",
   async (id, { rejectWithValue }) => {
     try {
       const { data } = await axios.delete(`/user/favorite/${id}`);
@@ -77,59 +78,72 @@ export const deleteFromFavorite = createAsyncThunk(
   }
 );
 
-export const updateInfoUser = createAsyncThunk(
-  'user/updateInfoUser',
-  async (upDateUser, { rejectWithValue, getState }) => {
+export const updateUser = createAsyncThunk(
+  "user/updateInfoUser",
+  async (data, { rejectWithValue }) => {
     try {
-      const value = getState().auth.accessToken;
-      if (value === null) {
-        return rejectWithValue('Unable to patch user');
-      }
-      accessToken.set(value);
-
-      const { data } = await axios.patch('/user/update', upDateUser);
       console.log(data);
-      return data;
-    } catch (error) {
-      return rejectWithValue(error);
+      const result = await api.updUserInfo(data);
+      return result;
+    } catch ({ responce }) {
+      return rejectWithValue(responce);
     }
   }
 );
 
-export const updateAvatar = createAsyncThunk(
-  'user/updateAvatar',
-  async (image, { rejectWithValue, getState }) => {
-    console.log(image);
+// export const updateInfoUser = createAsyncThunk(
+//   'user/updateInfoUser',
+//   async (upDateUser, { rejectWithValue, getState }) => {
+//     try {
+//       const value = getState().auth.accessToken;
+//       if (value === null) {
+//         return rejectWithValue('Unable to patch user');
+//       }
+//       accessToken.set(value);
 
-    const formData = new FormData();
-    formData.append('avatar', image);
+//       const { data } = await axios.patch('/user/update', upDateUser);
+//       console.log(data);
+//       return data;
+//     } catch (error) {
+//       return rejectWithValue(error);
+//     }
+//   }
+// );
 
-    try {
-      const value = getState().auth.accessToken;
-      if (value === null) {
-        return rejectWithValue('Unable to patch user');
-      }
-      accessToken.set(value);
-      const { data } = await axios.patch('/user/avatars', image, {
-        headers: {
-          'Content-type': 'multipart/form-data',
-        },
-      });
-      console.log(data);
-      return data;
-    } catch (error) {
-      return rejectWithValue(error);
-    }
-  }
-);
+// export const updateAvatar = createAsyncThunk(
+//   "user/updateAvatar",
+//   async (image, { rejectWithValue, getState }) => {
+//     console.log(image);
+
+//     const formData = new FormData();
+//     formData.append("avatar", image);
+
+//     try {
+//       const value = getState().auth.accessToken;
+//       if (value === null) {
+//         return rejectWithValue("Unable to patch user");
+//       }
+//       accessToken.set(value);
+//       const { data } = await axios.patch("/user/avatars", image, {
+//         headers: {
+//           "Content-type": "multipart/form-data",
+//         },
+//       });
+//       console.log(data);
+//       return data;
+//     } catch (error) {
+//       return rejectWithValue(error);
+//     }
+//   }
+// );
 
 export const deletePets = createAsyncThunk(
-  'user/deletePets',
+  "user/deletePets",
   async (id, { rejectWithValue, getState }) => {
     try {
       const value = getState().auth.accessToken;
       if (value === null) {
-        return rejectWithValue('Unable to patch user');
+        return rejectWithValue("Unable to patch user");
       }
       accessToken.set(value);
       const { data } = await axios.delete(`/pets/${id}`);
