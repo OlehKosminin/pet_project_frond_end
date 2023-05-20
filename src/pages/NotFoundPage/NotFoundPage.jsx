8c1e7066fe68ab98859c5f215b0518b66bb89b2f
DydikN <PingvinPain@gmail.com>
2023-05-20 15:43:30 +0300

    add img and styles for Not Found

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,30 @@
+import styles from "../../shared/components/sass/authPages.module.scss";
+import style from "./not-found-image.module.scss";
+import notFoundMobileImage from "../../assets/image/notFound/notFound-mobile@2x.png";
+import notFoundTabletImage from "../../assets/image/notFound/notFound-tablet@2x.png";
+import notFoundDesktopImage from "../../assets/image/notFound/notFound-desktop@2x.png";
+
 const NotFoundPage = () => {
-  return <h1>Not Found</h1>;
+  const screenWidth = window.innerWidth;
+
+  let notFoundImage;
+  if (screenWidth <= 767) {
+    notFoundImage = notFoundMobileImage;
+  } else if (screenWidth <= 1280) {
+    notFoundImage = notFoundTabletImage;
+  } else {
+    notFoundImage = notFoundDesktopImage;
+  }
+  return (
+    <div className={styles.page}>
+      <div className={styles.background}></div>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Ooops! This page not found :(</h1>
+
+        <img src={notFoundImage} alt="Not Found" className={styles.image} />
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
