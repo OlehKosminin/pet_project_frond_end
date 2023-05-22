4ec53b3fd05c26a4e3553261dd10aaceb56d92bd
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-05-22 19:52:51 +0300

    Йобана хуйня

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -4,13 +4,12 @@ import OurFriendsList from '../../shared/components/OurFriends/OurFriendsList/Ou
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFriends } from '../../redux/friends/friends-operations';
-import { selectIsisLoaded } from '../../redux/friends/friends-selector';
+import { getIsLoading } from '../../redux/friends/friends-selector';
 import styles from './styles';
 
 function OurFriendsPage() {
   const dispatch = useDispatch();
-
-  const isLoad = useSelector(selectIsisLoaded);
+  const isLoading = useSelector(getIsLoading);
 
   useEffect(() => {
     dispatch(getFriends());
@@ -22,7 +21,7 @@ function OurFriendsPage() {
         <Typography sx={styles.title} variant="h2">
           Our friends
         </Typography>
-        {isLoad ? <OurFriendsList /> : <Loader />}
+        {isLoading ? <OurFriendsList /> : <Loader />}
       </Container>
     </>
   );
