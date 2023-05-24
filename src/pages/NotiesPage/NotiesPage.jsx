6ff16c1d4891e6bb4a1db48adbff392bdb492ace
Vlad Pazyna <vladpazyna@gmail.com>
2023-05-24 19:11:21 +0300

    search

--- src/pages/NotiesPage/NotiesPage.jsx
+++ src/pages/NotiesPage/NotiesPage.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 
 import NoticesCategoriesList from "../../modules/Noties/NotiesCategoriesList/NotiesCategoriesList";
