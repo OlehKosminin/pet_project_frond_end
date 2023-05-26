4643d8bd61dffcb3ea26000b003c2afa76e9be12
Olehys <arzamazova13@gmail.com>
2023-05-26 12:52:01 +0300

    sda

--- src/shared/services/auth.js
+++ src/shared/services/auth.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
 
-  // baseURL: "https://pet-project-backend.onrender.com",
+  baseURL: "https://pet-project-backend.onrender.com",
 });
 
 const setToken = (token) => {
