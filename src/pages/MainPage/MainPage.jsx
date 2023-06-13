18e0b43538240020e0d38f5f78fb9f181291aea1
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2023-06-14 00:46:40 +0300

    new

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Container } from "@mui/material";
 import styled from "./MainPage.module.scss";
 
 function MainPage() {
   return (
     <>
-      <Container className={styled.mainPage}>
+      <div className={styled.mainPage}>
         <h3 className={styled.title}>
           Take good care of your small pets
         </h3>
-      </Container>
+      </div>
     </>
   );
 }
